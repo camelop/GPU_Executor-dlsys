f87c8e305c566b755c107d04d903f4935fb52d79
Little Round <lxy9843@qq.com>
2017-07-22 15:03:52 +0800

    312

--- src/gpu_op.cu
+++ src/gpu_op.cu
@@ -5,7 +5,7 @@
 #include <cuda_runtime.h>
 #include <stdio.h>
 
-#define THREADS_PER_BLOCK 256
+#define THREADS_PER_BLOCK 1
 
 /* TODO: Your code here */
 /* all your GPU kernel code, e.g. matrix_softmax_cross_entropy_kernel */
